993819dfc304594cef8eec6007a37535fe170af7
Marshall Asch <maasch@rogers.com>
2021-04-11 17:33:07 -0400

    added code linter to make formatting consistent

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // We need traceur to compile the ecmascript 6 code to ecmascript 5 so node can interpret it
-traceur = require('traceur');
-true_random = traceur.require(__dirname+'/es6/index.js');
+const traceur = require('traceur');
+
+const true_random = traceur.require('./es6/index.js');
+
 module.exports = true_random;
